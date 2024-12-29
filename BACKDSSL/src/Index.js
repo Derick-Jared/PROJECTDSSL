f80867f89a2a27a3ac22db72a6ad6f1cc8366411
Derick-Jared <108320862+Derick-Jared@users.noreply.github.com>
2024-12-29 13:48:51 -0500

    feat: index

---
+++ BACKDSSL/src/Index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const categoriaController = require('./controllers/CategoriaController');
+const productoController=require('./controllers/productoController');
+const detalleventaController=require('./controllers/detalleventaController');
+const ventaController=require('./controllers/ventaController');
+const usuarioController=require('./controllers/UsuarioController');
+const tipoUsuarioController=require('./controllers/TipoUsuarioController');
+const personaController=require('./controllers/PersonaController');
+
+
+const app =express();
+app.use(express.json());
+app.use(helmet());
+
+app.use(helmet.referrerPolicy({
+    policy: 'strict-origin-when-cross-origin'
+}));
+
+app.use((req, res,next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,OPTIONS');
+    next();
+})
+
+app.use('/api/categorias',categoriaController);
+app.use('/api/productos',productoController);
+app.use('/api/detalleventas',detalleventaController);
+app.use('/api/ventas',ventaController);
+app.use('/api/usuario',usuarioController);
+app.use('/api/tipousuario',tipoUsuarioController);
+app.use('/api/persona',personaController);
+
+
+const port=process.env.port || 3000;
+
+app.listen(port,()=>{
+    console.log(`servidor corriendo en http://localhost:${port}`); //AltGr+cierre de llave=comilla simple al reves
+})
