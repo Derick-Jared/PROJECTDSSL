4038014660ed21ef162a39d94919971f5c06654b
Derick-Jared <108320862+Derick-Jared@users.noreply.github.com>
2024-12-29 13:42:22 -0500

    deleted Producto Repository and services

--- BACKDSSL/src/repositories/ProductoRepository.js
+++
@@ -1,9 +0,0 @@
-const CrudRepository = require('../lib/crudRepository');
-const Producto = require('../models/Producto');
-
-class ProductoRepository extends CrudRepository{
-    constructor(){
-        super(Producto);
-    }
-}
-module.exports = new ProductoRepository();
