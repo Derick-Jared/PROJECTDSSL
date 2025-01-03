162bed4e061a63496992c7664962a0ccb88bff79
Derick-Jared <108320862+Derick-Jared@users.noreply.github.com>
2025-01-03 03:37:49 -0500

    feat: avance 1 front de gestion de registro

--- FRONTDSSL/src/app/app-routing.module.ts
+++ FRONTDSSL/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GestionregistroComponent } from "./components/gestionregistro/gestionregistro.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:'gestionregistro',component:GestionregistroComponent}, //NOMBRE A LLAMAR EN LA URL
+    {path:'',redirectTo:'/gestionregistro',pathMatch:'full'} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FRONTDSSL/src/app/app.module.ts
+++ FRONTDSSL/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GestionregistroComponent } from './components/gestionregistro/gestionregistro.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ClienteFormComponent } from './components/gestionregistro/cliente-form/cliente-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GestionregistroComponent,
+    ClienteFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
