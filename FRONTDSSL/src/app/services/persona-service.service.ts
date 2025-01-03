2564def8d3e3ae77e33d2b6fa479752e826abf3b
Derick-Jared <108320862+Derick-Jared@users.noreply.github.com>
2025-01-03 03:37:14 -0500

    feat: install bootstrap

---
+++ FRONTDSSL/src/app/services/persona-service.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Persona } from '../models/PersonaModel';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonaServiceService {
+  private apiUrl: string = 'http://localhost:3000/api/persona';
+
+  constructor( private http: HttpClient) { }
+
+  getPersons(): Observable<Persona[]> {
+    return this.http.get<Persona[]>(this.apiUrl);
+  }
+
+  getPerson(id: number): Observable<Persona> {
+    return this.http.get<Persona>(`${this.apiUrl}/${id}`);
+  }
+
+  createPerson(persona: Persona): Observable<Persona> {
+    return this.http.post<Persona>(this.apiUrl, persona);
+  }
+
+  updatePerson(id: number, persona: Persona): Observable<Persona> {
+    return this.http.put<Persona>(`${this.apiUrl}/${id}`, persona);
+  }
+
+  deletePerson(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+}
